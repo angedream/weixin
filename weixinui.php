bcf76ac06b7f6db6b11a58dd3cc293a0749f1360
zilong <angerdream@126.com>
2017-10-19 16:23:47 +0800

    weixin

---
+++ weixinui.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html style="height: 100%">
+    <head>
+        <meta charset="UTF-8">
+        <title>温度采集智能控制系统</title>
+    </head>
+    <body>
+        <h1>hello world !!!!!</h1>
+        <h2><?php echo $perName?></h2>
+    </body>
+</html>
